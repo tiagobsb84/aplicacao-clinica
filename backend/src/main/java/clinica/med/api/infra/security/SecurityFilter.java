2f30ca6e149babce99b18f79cefabb4738cd998a
Tiago Silva <tiagobsb84@gmail.com>
2024-01-16 13:54:32 -0300

    Validando o token recebido

--- backend/src/main/java/clinica/med/api/infra/security/SecurityFilter.java
+++ backend/src/main/java/clinica/med/api/infra/security/SecurityFilter.java
@@ -2,9 +2,11 @@ package clinica.med.api.infra.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import clinica.med.api.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,12 +14,17 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+	
+	@Autowired
+	private TokenService tokenService;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		
 		var tokenJWT = recuperarToken(request);
+		
+		var subject = tokenService.getSubject(tokenJWT);
+		System.out.println(subject);
 		
 		filterChain.doFilter(request, response);
 	}
@@ -32,3 +39,4 @@ public class SecurityFilter extends OncePerRequestFilter {
 	}
 
 }
+																																																																																																																																																																																																																																																																																																																																																																																										
--- backend/src/main/java/clinica/med/api/service/TokenService.java
+++ backend/src/main/java/clinica/med/api/service/TokenService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import clinica.med.api.domain.usuario.Usuario;
 
@@ -30,6 +31,19 @@ public class TokenService {
         } catch (JWTCreationException exception){
             throw new RuntimeException("erro ao gerrar token jwt", exception);
         } 
+	}
+	
+	public String getSubject(String tokenJWT) {
+        try {
+                var algoritmo = Algorithm.HMAC256(secret);
+                return JWT.require(algoritmo)
+                                .withIssuer("API Voll.med")
+                                .build()
+                                .verify(tokenJWT)
+                                .getSubject();
+        } catch (JWTVerificationException exception) {
+                throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
 	}
 
 	private Instant dataExpiracao() {
